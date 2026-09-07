d53fe17973225c0962f4e2f1bee793efaf06c4d5
hafshahluthfiah <hafshahluthfiah@gmail.com>
2026-09-07 11:34:40 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('islamic-memory-v2').then((cache) => {
+    caches.open('islamic-memory-v3').then((cache) => {
       return cache.addAll(['./', './index.html', './manifest.json']);
     })
   );
